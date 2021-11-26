e10f2cf00840aab20df0890f68d6f93d5ed69968
thomas210 <thomastabosa10@gmail.com>
2021-11-26 14:45:38 -0300

    :memo: add comments

--- src/main/java/com/cers/bulletinBoard/controller/NoticeController.java
+++ src/main/java/com/cers/bulletinBoard/controller/NoticeController.java
@@ -9,12 +9,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
+/**
+ * Notice Repository
+ */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200") // It was necessary to insert the front project url so that it can have permission to access the routes.
 @RequestMapping({"/notices"})
 public class NoticeController {
 
@@ -24,12 +25,25 @@ public class NoticeController {
         this.repository = noticeRepository;
     }
 
+    /**
+     * Method to return all warnings present in the system. Notices will be paged in sets of 5 notices and are
+     * sorted by creation date. Notices that have not yet been opened, that have the preview date filled in,
+     * are on the front, for the sake of importance.
+     * @param pageNumber Page number on pagination.
+     * @return List containing the initial information regarding pagination, how many pages it has,
+     * number of elements per page, current page, followed by the registered warnings.
+     */
     @GetMapping
     public Page<Notice> findAll(@RequestParam("page") int pageNumber){
         Pageable pagination = PageRequest.of(pageNumber, 5, Sort.by("visualizationDate"));
         return this.repository.findAll(pagination);
     }
 
+    /**
+     *Method for viewing a warning only. If the notice has not been opened yet, the viewing date is filled in.
+     * @param id of the Notice.
+     * @return the model Notice
+     */
     @GetMapping(path="/{id}")
     public ResponseEntity read(@PathVariable long id){
         return this.repository.findById(id)
@@ -42,12 +56,25 @@ public class NoticeController {
                 }).orElse(ResponseEntity.notFound().build());
     }
 
+    /**
+     * Method for creating the notice. The only mandatory field is the title.
+     * The creation date is populated based on the current date.
+     * @param model request of the Notice.
+     * @return
+     */
     @PostMapping
     public Notice create(@RequestBody Notice model){
         model.setPublicationDate(new Date());
         return this.repository.save(model);
     }
 
+    /**
+     * Method to update the notice. If the notice has already been opened then the
+     * display date field is cleared and it is treated as a new notice.
+     * @param id of the Notice
+     * @param model request of changes.
+     * @return
+     */
     @PutMapping(value = "/{id}")
     public ResponseEntity update(@PathVariable("id") long id, @RequestBody Notice model){
         return this.repository.findById(id)
@@ -60,6 +87,11 @@ public class NoticeController {
                 }).orElse(ResponseEntity.notFound().build());
     }
 
+    /**
+     * Method to delete the notice.
+     * @param id
+     * @return
+     */
     @DeleteMapping(path = {"/{id}"})
     public ResponseEntity <?> delete(@PathVariable long id) {
         return this.repository.findById(id)
